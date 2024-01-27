8d8977de255febc5ceca619074f499cd86c2e987
Abdul Samad Khan <abdulsamad31650@gmail.com>
2024-01-27 23:59:21 +0500

    Basic app completed Design is in progress

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,39 @@ function App() {
 				className=' w-full h-screen'
 				style={{ backgroundColor: color }}
 			>
+				<h3 className='text-center text-white text-3xl text-bold pt-10'>
+					Click on the Button to select Color
+				</h3>
 				<div className=' fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
-					<div className='flex flex-wrap justify-center gap-3 bg-white px-3 py-2 rounded 2xl'>
-						buttton
+					{/* <h2>test</h2> */}
+					<div className='flex flex-wrap justify-center text-white text-bold gap-3 bg-white px-3 py-2 rounded 3xl'>
+						<button
+							onClick={() => {
+								setColor('red');
+							}}
+							className='outline-none px-3 py-2 rounded-xl'
+							style={{ backgroundColor: 'red' }}
+						>
+							Red
+						</button>
+						<button
+							onClick={() => {
+								setColor('blue');
+							}}
+							className='outline-none px-3 py-2 rounded-xl'
+							style={{ backgroundColor: 'blue' }}
+						>
+							Blue
+						</button>
+						<button
+							onClick={() => {
+								setColor('green');
+							}}
+							className='outline-none px-3 py-2 rounded-xl'
+							style={{ backgroundColor: 'green' }}
+						>
+							Green
+						</button>
 					</div>
 				</div>
 			</div>
